ce46bd2ce97880dde007550b22eb08e4bda46328
ClassCat <support@classcat.com>
2015-07-29 00:28:33 +0900

    os_lib_agent.py fixed

--- main/webapp/os_lib_agent.py
+++ main/webapp/os_lib_agent.py
@@ -1,9 +1,30 @@
-#!/usr/bin/env python
+"""
+/* Copyright (C) 2006-2008 Daniel B. Cid <dcid@ossec.net>
+ * All rights reserved.
+ *
+ * This program is a free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 3) as published by the FSF - Free Software
+ * Foundation
+ */
+"""
+
+##############################################################
+# ClassCat(R) Prism for HIDS
+#  Copyright (C) 2015 ClassCat Co.,Ltd. All rights reseerved.
+##############################################################
+
+# === Notice ===
+# all python scripts were written by masao (@classcat.com)
+#
+# === History ===
+# 29-jul-15 : fixed for beta.
+#
 
 import time
 import subprocess
 
-def os_getagents(ossec_handle = None):
+def os_getagents(conf):
     agent_list = []
     agent_count = 0
 
@@ -15,8 +36,8 @@ def os_getagents(ossec_handle = None):
     agent_list[agent_count]['os'] = os.strip().decode('utf-8')
     agent_list[agent_count]['connected'] = 1
 
-    agent_count += agent_count
-
-    # print (ossec_handle['agent_dir'])
+    agent_count += 1
 
     return agent_list
+
+### End of Script ###
